431cc380370fedf7d9c21b96a5591f558cb3cdb1
Fahim-05 <fahim45147@gmail.com>
2022-09-21 07:18:56 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
+
+
+
+// create component
+/* function LoadCountries(){
+  const [countries, setCountries] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res=>res.json())
+    .then(data=>setCountries(data))
+  },[])
+
+  return(
+    <div>
+      <h1>Visiting Every Countriy of the World!!!</h1>
+      <h5>Available countries: {countries.length}</h5>
+      {
+        countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+      }
+    </div>
+  )
+}
+
+
+// country component
+function Country(props){
+  return (
+    <div>
+      <h3>Name: {props.name}</h3>
+      <h5>Population: {props.population}</h5>
+    </div>
+  )
+} */
 
 export default App;
